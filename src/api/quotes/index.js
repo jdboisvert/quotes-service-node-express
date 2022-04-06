a29f457f085b62ca7e4e7d6e9e855aa2647b4054
Jeffrey Boisvert <info.jeffreyboisvert@gmail.com>
2022-04-05 22:34:21 -0400

    Added update method and TODO for get by ID and delete.

--- src/api/quotes/index.js
+++ src/api/quotes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const quotes = require('../../models/quotes');
-const { quoteValidationRules, validate } = require('./validators.js');
+const { quoteValidationRules, quoteUpdateValidationRules, validate } = require('./validators.js');
 
-router.get('/', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
 	try {
-		res.json(quotes.getAll());
+		const { id } = req.params;
+
+		if (!id){
+			res.json(quotes.getAll());
+		}
+
+		// TODO Get by ID
+
 	} catch(err) {
 		console.error(`Error while getting quotes `, err.message);
 		next(err);
@@ -20,5 +27,23 @@ router.post('/', quoteValidationRules(), validate, (req, res) => {
 		console.error(`Error while adding a quote`, err.message);
 	}
 });
+
+router.put('/:id', quoteUpdateValidationRules(), validate, (req, res) => {
+	try {
+		const { id } = req.params;
+		const { quote, author } = req.body;
+		if (!quote && !author){
+			res.json("You must provide at least one value to update.");
+		}
+
+		res.json(quotes.update({ id, quote, author }));
+	} catch(err) {
+		console.error(`Error while adding a quote`, err.message);
+	}
+});
+
+// router.delete('/:id', (req, res, next) => {
+// 	// TODO Make delete
+// });
 
 module.exports = router;
--- src/models/quotes.js
+++ src/models/quotes.js
@@ -18,7 +18,36 @@ const create = (quoteObj) => {
 	return { message };
 };
 
+const update = (quoteObj) => {
+	const { id, quote, author } = quoteObj;
+
+	let result = null;
+	if (quote && author){
+		result = db.run(
+			'UPDATE quote SET (quote, author) VALUES (@quote, @author) WHERE id = @id', { quote, author, id }
+		);
+	}
+
+	if(quote && !author){
+		result = db.run('UPDATE quote SET (quote) VALUES (@quote) WHERE id = @id', { quote, id });
+	}
+
+	if (author && !quote){
+		result = db.run('UPDATE quote SET (author) VALUES (@author) WHERE id = @id', { author, id });
+	}
+	
+	let message = 'Error in creating quote';
+	if (result.changes) {
+	  message = 'Quote created successfully';
+	}
+  
+	return { message };
+};
+
+
+
 module.exports = {
-	getAll,
 	create,
+	update,
+	getAll,
 };
