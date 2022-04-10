1c5ae5ad06fccdfc81649c12b7d22aa1fcf98b0a
Jeffrey Boisvert <info.jeffreyboisvert@gmail.com>
2022-04-10 13:25:40 -0400

    Added logic to create the table for every DB call in case it does no exist.

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,36 @@
 const sqlite = require('better-sqlite3');
+const fs = require("fs");
 const path = require('path');
-const db = new sqlite(path.resolve('quotes.db'), { fileMustExist: true });
+const { logger } = require('../logger');
+
+const db = new sqlite.Database(path.resolve('quotes.db'),
+	sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE,
+	(error) => {
+		if (error){
+			return logger.error("Unable to read / create database.", error);
+		}
+		// Was able to connect ensure table is set up 
+		logger.info("Connected to the in-memory SQLite database successfully.");
+	}
+);
+
+const setUpTable = () => {
+	const createTableSQL = fs.readFileSync("../../sql/create_quotes_table.sql").toString();	
+	db.prepare(createTableSQL).run();
+};
   
 const run = (sql, params) => {
+	setUpTable();
 	return db.prepare(sql).run(params);
 };
 
 const runQueryWithoutParams = (sql) => {
+	setUpTable();
 	return db.prepare(sql).all();
 };
 
 const queryWithParams = (sql, params) => {
+	setUpTable();
 	return db.prepare(sql).all(params);
 };
 
