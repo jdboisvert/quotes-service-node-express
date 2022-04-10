c38e97a25fa73eb1e2ea6ecf03f5640508da4910
Jeffrey Boisvert <info.jeffreyboisvert@gmail.com>
2022-04-10 13:39:41 -0400

    Added basic endpoint tests for quotes with jest.

---
+++ tests/api/index.test.js
@@ -0,0 +1,29 @@
+const { describe, it } = require("eslint/lib/rule-tester/rule-tester");
+
+describe("Quote Endpoints", () => {
+    it("Should test that all quotes in the database are returned"), () => {
+        // TODO
+    }); 
+    it("Should test that all quotes in the database are returned (when none exist)"), () => {
+        // TODO
+    }); 
+
+
+    it("Should test that a specific quote in the database is returned"), () => {
+        // TODO
+    });
+
+    it("Should test that a quote is inserted in the database"), () => {
+        // TODO
+    });
+    it("Should test that a quote is not inserted in the database since it already exists"), () => {
+        // TODO
+    });
+
+    it("Should test that a quote is updated in the database"), () => {
+        // TODO
+    });
+    it("Should test that a quote is not updated in the database since it does not exist"), () => {
+        // TODO
+    });
+});
